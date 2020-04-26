8e158164ea42fc49769f100cff6fc1d37f24b670
alimohseni <mohseni676@gmail.com>
2020-04-26 17:12:59 +0430

    BuidGradle modified

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           fontFamily: 'Yekan',
           primarySwatch: Colors.deepOrange,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+          //visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: MainPage());
   }
